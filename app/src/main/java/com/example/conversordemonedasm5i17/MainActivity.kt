fee50a373d8d689ea6326d8b0da2e48cc8db9efa
Enzo_Gatica <enzogatica96@gmail.com>
2023-07-20 20:41:55 -0400

    CheckPoint, divisas

--- app/src/main/java/com/example/conversordemonedasm5i17/MainActivity.kt
+++ app/src/main/java/com/example/conversordemonedasm5i17/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.conversordemonedasm5i17
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.conversordemonedasm5i17.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val divisas = listOf<String>("Dolar", "Pesos", "Euro")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.spinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, divisas)
+        binding.spinner2.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, divisas)
     }
 }
